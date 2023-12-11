d4ca644e3db3c4f3c60e9b6aeb236ddb7a167e91
kevinccbsg <kevinccbsg@gmail.com>
2023-12-11 20:30:38 +0100

    feat: add object examples

--- examples/07_objects.js
+++ examples/07_objects.js
@@ -64,3 +64,106 @@ Owners: ${fordMustang.owners.join(' | ')}
 `;
 
 console.log(paginadeProducto);
+
+// ??
+console.log(fordMustang['second-owner']);
+console.log(fordMustang['year']);
+
+const key = 'year'; // prompt('Que info quieres saber del fordMustang?');
+/*
+if (key === 'year') {
+  console.log(fordMustang.year);
+} else if (key === 'model') {
+  console.log(fordMustang.model);
+*/
+// key = 'country.name'
+console.log(fordMustang[key]);
+console.clear();
+
+const keyId = 'newKey2';
+
+const car = {
+  title: 'Car 1',
+  year: 1990,
+  [keyId]: 12,
+};
+
+console.log(car);
+
+console.log(car[keyId]);
+
+car.title = 'Car 2';
+car.year = 1970;
+
+car.title = car.title.toUpperCase();
+
+delete car.year;
+
+const removeKeys = ['title'];
+for (let index = 0; index < removeKeys.length; index++) {
+  const keyToRemove = removeKeys[index];
+  delete car[keyToRemove];
+}
+
+// info
+const shows = [
+  {
+    id: 1,
+    name: 'Under the dome',
+    genres: ['Drama', 'Science-Fiction', 'Thriller']
+  }, /// 0
+  {
+    id: 2,
+    name: 'Game of thrones',
+    genres: ['Drama', 'Thriller'],
+    season: {
+      title: 'season 1'
+    },
+  } /// 1
+];
+
+let showTemplate = '';
+
+for (let index = 0; index < shows.length; index++) {
+  const show = shows[index];
+  // mostrar
+  showTemplate = showTemplate + `
+    <h2>${show.name}</h2>
+    <p>${show.genres.join(', ')}</p>
+  `;
+}
+// renderizar
+document.write(showTemplate);
+
+/// Object.keys, Object.values, Object.entries for..in, JSON.
+
+const stringValue = JSON.stringify(shows);
+const showsCopy = shows;
+console.log(showsCopy === shows);
+console.log(shows);
+console.log(stringValue);
+const newShowsArray = JSON.parse(stringValue);
+console.log(newShowsArray);
+
+car.year = undefined;
+
+console.log(JSON.parse(JSON.stringify(car)));
+
+console.log('Keys ', Object.keys(shows[1]));
+console.log('Values:', Object.values(shows[1]));
+
+console.log('Entries: ', Object.entries(shows[1]));
+
+console.clear();
+for (let key in shows[1]) {
+  console.log(key);
+  const value = shows[1][key];
+  console.log(value);
+  if (key === 'name') {
+    shows[1][key] = '***';
+  }
+}
+
+console.log('Keys ', Object.keys(shows));
+
+console.log(shows);
