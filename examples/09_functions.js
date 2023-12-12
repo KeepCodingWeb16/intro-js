2a09ce700c73215a722a00d828baf1381888be8d
kevinccbsg <kevinccbsg@gmail.com>
2023-12-12 20:26:46 +0100

    feat: add function examples

--- examples/09_functions.js
+++ examples/09_functions.js
@@ -53,10 +53,136 @@ for (let { age, name } of users) {
   }
 }
 
-function sum(value1 = 0, value2 = 0) {
-  return value1 + value2;
+function sum(value1 = 0, value2 = 0, value3 = 0) {
+  return value1 + value2 + value3;
 }
 
-console.log(sum());
+const suma = function (value1 = 0, value2 = 0, value3 = 0) {
+  return value1 + value2 + value3;
+};
 
+// console.log(suma(1, 2));
+
+const crearSaludo = function (nombre) {
+  console.log('Se ejecuta?');
+  return `Hola ${nombre}`;
+};
+
+const saludoPersona = crearSaludo;
+
+console.log(saludoPersona('Persona 1'));
+
+// arrow => 
+/*
+const sumArrow = (value1 = 0, value2 = 0, value3 = 0) => (
+  value1 + value2 + value3
+);
+*/
+const getAdmin = () => (
+  {
+    name: 'Keepcoding',
+    role: 'ADMIN',
+  }
+);
+
+// console.log(sumArrow(0, 2));
+console.log(getAdmin());
 console.log('Final');
+console.clear();
+
+/// this function vs arrow
+
+const obj = {
+  value: 20,
+  noArrow: function () {
+    console.log('No arrow function', this.value);
+    function insideFunction() {
+      console.log('Inside function', this.value);
+    }
+
+    insideFunction()
+  }
+};
+
+console.log(obj.noArrow());
+
+/// rest params
+
+const sumArrow = (...params) => {
+  let result = 0;
+  for (let num of params) {
+    result += num;
+  }
+  return result;
+};
+
+console.log(sumArrow(1, 2, 3));
+
+const adminUser = {
+  name: 'Keepcoding',
+  role: 'ADMIN',
+  password: 'wqerty23123',
+  courses: [],
+  age: 28,
+  active: true,
+};
+
+/*
+const getUserInfo = (user) => {
+  // ??
+  const newUser = structuredClone(user);
+  delete newUser.password;
+  return newUser;
+};
+*/
+
+const getUserInfo = ({ password, ...otrasProps }) => otrasProps;
+
+
+const adminInfo = getUserInfo(adminUser);
+console.log(adminUser);
+console.log(`usuario name: ${JSON.stringify(adminInfo)}`);
+
+
+let temperatura = parseInt(prompt("Ingresa la temperatura: "));
+let escala = prompt(
+  "Ingresa la escala a la que quieras cambiar tu temperatura (C o K): "
+)
+  .trim()
+  .toString()
+  .toUpperCase();
+
+const conversorTemp = (temp, escala) => {
+  if (isNaN(temperatura)) {
+    return "La temperatura introducida no es válida";
+  } else {
+    if (escala === "C") {
+      conversion = temperatura - 273.5;
+      return `La temperatura en Celsius es: ${conversion}°C`;
+    } else if (escala === "K") {
+      conversion = temperatura + 273.5;
+      return `La temperatura en Kelvin es: ${conversion}K`;
+    } else {
+      return "Error de escala";
+    }
+  }
+};
+
+let numeros = [1, 2, 33, 41, 5, 60, 74, 87, 90, 101, null, '', false]
+
+const calculNum = (list, parOrImp) => {
+  let impares = [];
+  let pares = [];
+  for (let index = 0; index < list.length; index++) {
+    const numero = list[index];
+    if (typeof numero === 'number') {
+      if (numero % 2 != 0) {
+        impares.push(numero)
+      } else {
+        pares.push(numero)
+
+      }
+    }
+  }
+  return (parOrImp === "par" ? pares :  impares);
+}
